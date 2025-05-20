cfa0860747ca0f433e2aedd206f2f4a196008a1f
anis <anis.siam@hotmail.com>
2025-05-20 14:08:38 +0300

    Java main point Java Identifiers Data type

---
+++ src/FirstClass.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+
+public class FirstClass {
+
+        //method syntax
+    // access modefire    // no access mod   // return type   //method name or function name  (parameter or argument)
+  //header
+    public static void main(String[] args) {
+//body
+
+       //System.out.println("Hello from console");  // call function   //invoke
+
+        JOptionPane.showMessageDialog(null , "Hello  from view ");
+     /*   System.out.println("anis");
+        System.out.println("siam");
+        System.out.print("good ");
+        System.out.println("person");*/
+
+
+ /*
+
+  4 print command
+ anis Siam
+ age : 31
+ major
+ CS
+
+  */
+
+
+
+
+        // var declaration
+        /*
+        1. type
+        2. name
+        3. value
+        4. size
+
+        // Ram --> Heap
+         */
+       byte  firstVar=5; // 8 bit // 1 byte    // -128 : 127
+        /*
+        data type
+
+        1. Primitive
+            1.Numaric
+                1. Integers // الارقام الصحيحة
+                    1. byte  //1 byte  // 8 bit
+                    2. short  // 2 byte // 16 bit
+                    3. int     // 4 byte // 32 bit
+                    4. long     // 8 byte // 64 bit
+                2.Floating point // الارقام العشرية
+                    1. float
+                    2. double
+
+            2. Non Num
+                1. char  //الحروف
+                2. boolean  // true false
+
+        2. Non - Primitive
+            class
+            string
+            array
+            object
+         */
+
+        int x ;  // null   // local var // scoop
+        x = 10;
+
+        System.out.print(x);
+
+
+
+
+
+
+
+
+    }
+
+
+    public void showAge(){
+
+        int x = 10;
+
+         x = 50;
+
+    }
+
+}
+
